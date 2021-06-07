2d2ecae621a368ad184233c227a82fb7be41245a
Matheus Vieira <matheustsr@outlook.com>
2021-06-07 07:34:58 -0300

    mongoose configured

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import mongoose from './mongoose'
 import routes from './routes';
 
 class App{
 
   constructor(){
     this.server = express();
+
+    mongoose.connect('mongodb+srv://admin:admin@cluster0.mt7qk.mongodb.net/devhouse?retryWrites=true&w=majority', {
+      useNewUrlParser: true, //Configure to a new URL format
+      useUnifiedTopology: true, // keep a stable connection
+    });
 
     this.middlewares();
     this.routes();
