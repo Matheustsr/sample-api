490002ed030db8c1748d893976e2014538f85550
Matheus Vieira <matheustsr@outlook.com>
2021-06-07 07:10:39 -0300

    sucrase refactor

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
-const { Router } = require('express')
+import { Router } from 'express'
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
     return res.json({ok: true})
-
 })
 
-module.exports = routes;
+export default routes;
