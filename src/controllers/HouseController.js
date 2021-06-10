d4f43447cc0f6bc701273352a560cdccc3a414dd
Matheus Vieira <matheustsr@outlook.com>
2021-06-10 07:13:03 -0300

    rest complete

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -2,14 +2,15 @@ import House from '../models/House'
 
 class HouseController{
 
-    async index(req, res){
-        return res.json({
-            ok:true
+    async index(req, res){ // getting a house filtering by status
+        const { status } = req.query;
 
-        })
+        const houses = await House.find({ status }) // find by 'status' 
+
+        return res.json(houses);
     }
 
-    async store(req, res){ // getting data 
+    async store(req, res){ // creating a house 
         const { filename } = req.file
         const { description, price, location, status } = req.body
         const { user_id } = req.headers
@@ -24,6 +25,31 @@ class HouseController{
         });
         
         return res.json(house);
+    }
+
+    async update(req, res){ //update a house
+
+        const { location } = req.query;
+        const { newLocation } = req.headers;
+        const houses = await House.find({ location });
+
+        const house = await House.updateOne({
+            houses: newLocation,
+        });
+        return res.json(house);
+            
+    }
+
+    async delete(req, res){ // delete a house
+
+        const { location } = req.query;
+        const housess = await House.find({ location });
+
+        const houses = await House.deleteOne({ location }) // delete by 'location' 
+
+        console.log('deletou')
+
+
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,8 @@ const upload = multer(uploadConfig)
 
 routes.post('/sessions', SessionController.store);
 routes.post('/houses', upload.single('thumbnail'), HouseController.store);
+routes.get('/houses', HouseController.index);
+routes.put('/houses', HouseController.update);
+routes.delete('/houses', HouseController.delete);
 
 export default routes;
