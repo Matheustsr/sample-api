0ce1f331ed4aa1134a19c57ed97c5a16e467b4bb
Matheus Vieira <matheustsr@outlook.com>
2021-06-11 06:05:15 -0300

    Index/Destroy ReserveController

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -1,11 +1,39 @@
 import Reserve from '../models/Reserve';
+import User from '../models/User';
+import House from '../models/House';
 
 class ReserveController{
+
+    async index(req, res){ // list reserves
+
+        const { user_id } = req.headers;
+
+        const reserve =  await Reserve.find({ user: user_id }).populate('house');
+
+    }
     async store(req, res){
         
         const { user_id } = req.headers;
         const { house_id } = req.params;
         const { date } = req.body;
+
+
+        
+        const house = await House.findById(house_id);
+        if(!house){
+            return res.status(400).json({ error: 'Casa inexistente!'});
+        }
+
+        if(house.status !== true){
+            return res.status(400).json({ error: 'Casa indisponivel!'});
+        }
+
+        const user = await User.findById(user_id);
+
+        if(String(user._id) === String(house.user)){
+            return res.status(401).json({ error: 'Reserva não permitida!'});
+        }
+
 
 
         const reserve = await Reserve.create({
@@ -15,9 +43,18 @@ class ReserveController{
             date, 
         })
 
-        await reserve.populate('house').populate('user').execPopulate();
+        await reserve.populate('house').populate('user').execPopulate(); // add more information to  reserve 
 
         return res.json(reserve);
+    }
+
+    async destroy(req, res){
+
+        const { reserve_id } = req.body;
+
+        await Reserve.findByIdAndDelete({ _id: reserve_id});
+        
+        return res.status(204).send();
 
     }
 
--- src/routes.js
+++ src/routes.js
@@ -12,12 +12,16 @@ const routes = new Router();
 const upload = multer(uploadConfig)
 
 routes.post('/sessions', SessionController.store); 
+
 routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.get('/houses', HouseController.index);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
 
 routes.get('/dashboard', DashboardController.show);
+
 routes.post('/houses/:house_id/reserve', ReserveController.store);
+routes.get('/reserves', ReserveController.index);
+routes.delete('/reserves/cancel', ReserveController.destroy);
 
 export default routes;
